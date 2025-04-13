96af64346ce2deaa8fe31bce8ec441a2803e30bf
Darya <dashkabulycheva@gmail.com>
2025-04-13 18:26:15 +0300

    исправление задания 1

--- Homework_1/Homework_1_1.java
+++ Homework_1/Homework_1_1.java
@@ -4,8 +4,7 @@ public class Homework_1_1 {
     public static void main(String[] args) {
         int b = 25;
         int c = 10;
-        int a;
 
-        System.out.println(a = 4 * (b + c - 1) / 2);
+        System.out.println(4 * (b + c - 1) / 2);
     }
 }
