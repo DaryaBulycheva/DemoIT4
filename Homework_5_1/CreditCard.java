18799ca94034b31aea79936dab5d45461da912ef
Darya <dashkabulycheva@gmail.com>
2025-05-03 19:32:04 +0300

    домашка 5

---
+++ Homework_5_1/CreditCard.java
@@ -0,0 +1,22 @@
+package Homework_5_1;
+
+public class CreditCard {
+    int number;
+    int total;
+
+    //метод для добавления денег на карту
+    public void addCash(int value) {
+        total = total + value;
+    }
+
+    //метод для снятия денег с карты
+    public void subCash(int value) {
+        total = total - value;
+    }
+
+    //метод для возврата текущей информации
+    public void showInfo() {
+        System.out.println("Номер карты = " + number);
+        System.out.println("Текущее состояние карты = " + total);
+    }
+}
